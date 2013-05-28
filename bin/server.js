b0feebb2e9acfd92194572cb6eea17c569216151
Mark Wolfe <mark@wolfe.id.au>
2013-05-28 14:16:01 +1000

    Added cleanup socket code.

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,24 @@
-
+var fs = require('fs')
 var UnixMuxDemux = require('../index.js')
 var unixMuxDemux = new UnixMuxDemux()
 
 var server = unixMuxDemux.createServer()
+
+
+function cleanup(){
+  console.log('Cleaning up socket', unixMuxDemux._pipe)
+  if (fs.existsSync(unixMuxDemux._pipe)) fs.unlinkSync(unixMuxDemux._pipe)
+}
+
+// hit ctrl-c
+process.on('SIGINT', function () {
+  cleanup()
+  process.exit(0)
+})
+
+// kill $pid
+process.on('SIGTERM', function () {
+  cleanup()
+  process.exit(0)
+})
+
